d6858bcd71d1e1ccdc6ffee1392a93008f55d9b1
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-10-17 19:50:01 -0400

    Add new variables for Azure storage configuration

--- variable.tf
+++ variable.tf
@@ -7,6 +7,27 @@ variable "client_id"{
 variable "client_secret"{
   type=string
 }
-variable "tenant_id"{
+variable "storage_account_name"{
   type=string
+  default="mcitoctstorage"
+}
+variable "resource_group_name"{
+  type=string
+  default="azurerm_resource_group.rgoctobermcit.name"
+}
+variable "location"{
+  type=string
+  default="azurerm_resource_group.rgoctobermcit.location"
+}
+variable "account_tier"{
+  type=string
+  default="Standard"
+}
+variable "account_replication_type" {
+  type=string
+  default="GRS"
+}
+variable "environment"{
+  type=string
+  default="staging"
 }
