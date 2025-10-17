c0e7d423f305acc44878faf4c2b46334fe112d71
Sami Islam <islam.samipkr@gmail.com>
2025-10-17 18:37:08 -0400

    Delete unused resource group for November
    Removed unused Azure resource group for November.

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -2,7 +2,3 @@ resource "azurerm_resource_group" "rgoctobermcit" {
   name     = "mcit_rg_october172025"
   location = "canadacentral"
 }
-resource "azurerm_resource_group" "rgnovembermcit" {
-  name     = "mcit_rg_nov172025"
-  location = "canadacentral"
-}
