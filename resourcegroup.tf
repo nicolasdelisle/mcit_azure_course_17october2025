ebdf28b0f32cac2b4fc828a8e31be29150482814
Sami Islam <islam.samipkr@gmail.com>
2025-10-17 18:34:21 -0400

    Add new Azure resource group for November

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rgoctobermcit" {
   name     = "mcit_rg_october172025"
   location = "canadacentral"
 }
+resource "azurerm_resource_group" "rgnovembermcit" {
+  name     = "mcit_rg_nov172025"
+  location = "canadacentral"
+}
