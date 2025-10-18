21da53a9c3881c3fac31ae226274db36bfd33ac6
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-10-17 20:12:52 -0400

    Add Azure Storage Account resource configuration

--- storage.tf
+++ storage.tf
@@ -1,1 +1,12 @@
+resource "azurerm_storage_account" "azurestoagemcit" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rgoctobermcit.name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 
+  tags = {
+    environment = var.environment
+  }
+}
+#ccc
