daa8b79437b8052f16dbe311fc4e8fd5b95b8276
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-10-17 20:06:51 -0400

    Update resource group reference for storage account

--- storage.tf
+++ storage.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "azurestoagemcit" {
   name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = azurerm_resource_group.mcitrgnicolas.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
