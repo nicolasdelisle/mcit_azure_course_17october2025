a6d504595f2dc625c84b9de930416e6a20a0197b
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-10-20 20:09:56 -0400

    Change lookup variable from 'dev' to 'prod'

--- output.tf
+++ output.tf
@@ -144,7 +144,7 @@ output "countries_joined" {
  
 # MCIT Lookup Exercise 1: Fetch instance type for 'dev'
 output "env_dev_instance" {
-  value = lookup(var.env_settings, "dev")
+  value = lookup(var.env_settings, "prod")
 }
 
 # MCIT Lookup Exercise 2: Fetch instance for 'prod', default to 'default_instance' if not found
