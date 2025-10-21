40963e867e9becb92906d6c528bdfe41aed6e576
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-10-20 20:00:18 -0400

    Define multiple new variables in variable.tf
    Added variables for environment settings, city codes, country currencies, and request cities.

--- variable.tf
+++ variable.tf
@@ -69,3 +69,40 @@ variable "department" {
 variable "skills_string" {
   default = "Terraform,Docker,Kubernetes,Python"
 }
+# -------------------
+# Variables for lookup practice
+# -------------------
+variable "env_settings" {
+  description = "Map of environment -> instance type"
+  default = {
+    dev   = "t2.micro"
+    qa    = "t3.small"
+    stage = "t3.medium"
+    prod  = "m5.large"
+  }
+}
+ 
+variable "city_codes" {
+  description = "Map of city -> airport IATA code"
+  default = {
+    Vancouver = "YVR"
+    Toronto   = "YYZ"
+    Montreal  = "YUL"
+    Calgary   = "YYC"
+  }
+}
+ 
+variable "country_currency" {
+  description = "Map of country -> currency code"
+  default = {
+    Canada = "CAD"
+    USA    = "USD"
+    Mexico = "MXN"
+  }
+}
+ 
+variable "request_cities" {
+  description = "Cities to resolve into airport codes for exercise 8"
+  default     = ["Vancouver", "Calgary", "Ottawa"]
+}
+ 
