669bf421dabb42988655c89a003e374360615c2c
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-10-24 18:58:58 -0400

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -12,13 +12,13 @@ resource "azurerm_storage_account" "azurestoagemcit" {
 
 resource "azurerm_storage_container" "storagecontainermay" {
   name                  = var.storagecontainermay
-  storage_account_id    = azurerm_storage_account.mcitstorage.id
+  storage_account_id    = azurerm_storage_account.mcitoctstorage.id
   container_access_type = var.container_access_type
 }
 
 resource "azurerm_storage_blob" "storageblobmay" {
   name                   = var.storageblobmay
-  storage_account_name   = azurerm_storage_account.mcitstorage.name
+  storage_account_name   = azurerm_storage_account.mcitoctstorage.name
   storage_container_name = azurerm_storage_container.storagecontainermay.name
   type                   = var.blob_type
   source                 = var.blob_source
