27c2984d3d63b3ff8280d0840eee2c90d4030651
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-10-24 20:00:41 -0400

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "storageaccount" {
   name                     = "${var.name}${var.storage_account_name}"
-  resource_group_name      = azurerm_resource_group.mcitrgnicolas.name
+  resource_group_name      = azurerm_resource_group.rgoctobermcit.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
