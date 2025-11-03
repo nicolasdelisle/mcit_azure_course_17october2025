5be9537d5e2dadbf51f30be3b4df94d318af81a9
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-11-03 18:27:41 -0500

    Update 3november.tf

--- 3november.tf
+++ 3november.tf
@@ -1,1 +1,84 @@
+# 5 vm created dynamicly
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = [var.vnet_address_space]
+  location            = var.resource_group_location
+  resource_group_name = var.second_resource_group_name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = var.second_resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_prefix]
+}
+
+resource "azurerm_public_ip" "pip" {
+  for_each            = toset(var.vm_names)
+  name                = "${each.key}-pip"
+  location            = var.resource_group_location
+  resource_group_name = var.second_resource_group_name
+  allocation_method   = var.public_ip_allocation_method
+}
+
+resource "azurerm_network_interface" "nic" {
+  for_each            = toset(var.vm_names)
+  name                = "${each.key}-nic"
+  location            = var.resource_group_location
+  resource_group_name = var.second_resource_group_name
+
+  ip_configuration {
+    name                          = "${each.key}-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = var.private_ip_allocation
+    public_ip_address_id          = azurerm_public_ip.pip[each.key].id
+  }
+}
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsg_name
+  location            = var.resource_group_location
+  resource_group_name = var.second_resource_group_name
+
+  security_rule {
+    name                       = var.nsg_rule_name
+    priority                   = var.nsg_rule_priority
+    direction                  = var.nsg_rule_direction
+    access                     = var.nsg_rule_access
+    protocol                   = var.nsg_rule_protocol
+    source_port_range           = var.nsg_rule_source_port
+    destination_port_range      = var.nsg_rule_destination_port
+    source_address_prefix       = var.nsg_rule_source_address
+    destination_address_prefix  = var.nsg_rule_destination_address
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  for_each = toset(var.vm_names)
+
+  name                  = each.key
+  resource_group_name   = var.second_resource_group_name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+  network_interface_ids = [azurerm_network_interface.nic[each.key].id]
+
+  os_disk {
+    caching              = var.os_disk_caching
+    storage_account_type = var.os_disk_type
+  }
+
+  source_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
+}
 
