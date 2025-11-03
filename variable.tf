cb68ddbf7f590241113b00598099b858360b0e49
nicolasdelisle <nicolas.delisle@hotmail.ca>
2025-11-03 18:29:54 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 # class november 3
 variable "vm_names" {
   description = "List of VM names to create"
-  type        = list, string
+  type        = list string
   default     = ["msuburb1", "msuburb2", "msuburb3", "msuburb4", "msuburb5"]
 }
 # class october 31
